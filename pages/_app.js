b6142f49cd77dca843de7d7f56f814dba88e917a
luaialaghbari <louiyaseralag@gmail.com>
2026-02-16 00:47:04 +0300

    Updates

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/style.css';
-import '../styles/mediaqueries.css';
-import '../styles/mobile.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
